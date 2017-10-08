78e9aba70203e8ab3bf2df505269fdfc5ae0dbe2
jhpgd5 <jhpgd5@mail.umkc.edu>
2017-10-08 15:10:43 -0500

    Sprint #2

--- src/main/java/com/example/jp048220/tradingcardsapp/MainActivity.java
+++ src/main/java/com/example/jp048220/tradingcardsapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jp048220.tradingcardsapp;
 
+import android.support.design.internal.NavigationMenuView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout menuDrawerLayout;
     private ActionBarDrawerToggle menuToggle;
+    NavigationMenuView navigationMenuView;
 
 
     @Override
@@ -24,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         menuToggle.syncState();
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
     }
 
     @Override
---
+++ src/main/java/com/example/jp048220/tradingcardsapp/profile.java
@@ -0,0 +1,17 @@
+package com.example.jp048220.tradingcardsapp;
+
+import android.os.Bundle;
+
+/**
+ * Created by JP048220 on 10/8/2017.
+ */
+
+public class profile extends MainActivity {
+
+    public void onCreateProfile(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+        setTitle("My Profile");
+
+    }
+}
