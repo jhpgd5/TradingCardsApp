bad88debc7e7857ec33f47f7af640a76573fda5a
jhpgd5 <jhpgd5@mail.umkc.edu>
2017-11-05 21:17:01 -0600

    Sprint #4

--- src/main/java/com/example/jp048220/tradingcardsapp/search.java
+++ src/main/java/com/example/jp048220/tradingcardsapp/search.java
@@ -7,11 +7,22 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.app.SearchManager;
+import android.widget.SearchView.OnQueryTextListener;
+import android.content.Context;
+
 
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by JP048220 on 10/22/2017.
@@ -22,11 +33,24 @@ public class search extends AppCompatActivity
 
 {
 
-    MaterialSearchView searchView;
+    ListView search_card;
+    ArrayAdapter<String> adapter;
+
+
+   // MaterialSearchView searchView;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search);
+
+        search_card=(ListView) findViewById(R.id.search_card);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.addAll(Arrays.asList(getResources().getStringArray(R.array.my_cards)));
+
+        adapter = new ArrayAdapter<String>(search.this, android.R.layout.simple_list_item_1, arrayList);
+
+        search_card.setAdapter(adapter);
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -37,7 +61,7 @@ public class search extends AppCompatActivity
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
         BotNavHelper.disableShiftMode(bottomNavigationView);
         Menu menu = bottomNavigationView.getMenu();
-        MenuItem menuItem = menu.getItem(2);
+        MenuItem menuItem = menu.getItem(1);
         menuItem.setChecked(true);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -50,10 +74,7 @@ public class search extends AppCompatActivity
                         startActivity(intent0);
 
                         break;
-                    case R.id.ic_history:
-                        Intent intent1 = new Intent(search.this, history.class);
-                        startActivity(intent1);
-                        break;
+
                     case R.id.ic_search:
 
                         break;
@@ -72,11 +93,33 @@ public class search extends AppCompatActivity
         });
 
     }
+
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_item, menu);
-        MenuItem item = menu.findItem(R.id.action_search);
-        searchView.setMenuItem(item);
-        return true;
+   public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+        MenuItem item = menu.findItem(R.id.search_card);
+        SearchView searchView = (SearchView)item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                try {
+                    adapter.getFilter().filter(newText);
+                }
+                catch(Exception e) {
+                    System.out.print("Error");
+                }
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+
     }
 }
